3b7e00200167d2dc0af9ab623a27dcbf4f8fc5a0
Dima Zapolsky <dima_zapolsky@mail.ru>
2019-02-20 23:11:47 +0300

    Working version

--- backup.sh
+++ backup.sh
@@ -3,11 +3,9 @@ rmdir $1$2/ 2> /dev/null
 mkdir $1$2/
 for (( c=3; c<=$#; c++ ))
 do
-	find /Users/dima_zapolsky/Desktop/ -name "*${!c}" -exec cp {} ${1}${2} ";" 2> /dev/null
+	find ~/ -name "*${!c}" -exec cp --backup=numbered {} ${1}${2} ";" 2> /dev/null
 done
 cd $1$2/
-tar -zcvf $1$2.tar.gz *
+tar -zcf $1$2.tar.gz * 2> /dev/null
 rm -r $1$2/
 echo done
-
-
